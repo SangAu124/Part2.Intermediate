db878dd2293a729a6d61118ab2de6e32df1cb8fe
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-24 10:52:24 +0900

    .
    .

---
+++ DrinkWater/DrinkWater/AlertListCell.swift
@@ -0,0 +1,29 @@
+//
+//  AlertListCell.swift
+//  DrinkWater
+//
+//  Created by 김상은 on 2021/12/24.
+//
+
+import UIKit
+
+class AlertListCell: UITableViewCell {
+
+    @IBOutlet weak var meridiemLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var alertSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func alertSwitchValueChanged(_ sender: UISwitch) {
+    }
+}
