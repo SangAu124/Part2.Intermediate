f93b6baf6b0f08eb6a9a1a381bd9970b18c031a9
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-24 11:28:22 +0900

    .
    .

--- DrinkWater/DrinkWater/AlertListViewContoller.swift
+++ DrinkWater/DrinkWater/AlertListViewContoller.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AlertListViewContoller: UITableViewController {
     
-    var alertList: [Alert] = []
+    var alerts: [Alert] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,14 +18,45 @@ class AlertListViewContoller: UITableViewController {
         tableView.register(nibName, forCellReuseIdentifier: "AlertListCell")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        alerts = alertList()
+    }
+    
     @IBAction func addAlertButtonAction(_ sender: UIBarButtonItem) {
+        guard let addAlertVC = storyboard?.instantiateViewController(withIdentifier: "AddAlertViewController") as? AddAlertViewController else { return }
+        
+        addAlertVC.pickedDate = { [weak self] date in
+            guard let self = self else { return }
+            
+            var alertList = self.alertList()
+            let newAlert = Alert(date: date, isOn: true)
+            
+            alertList.append(newAlert)
+            alertList.sort{ $0.date < $1.date}
+            
+            self.alerts = alertList
+            
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.alerts), forKey: "alerts")
+            
+            self.tableView.reloadData()
+        }
+        
+        self.present(addAlertVC, animated: true, completion: nil)
+    }
+    
+    func alertList() -> [Alert] {
+        guard let data = UserDefaults.standard.value(forKey: "alerts") as? Data,
+              let alerts = try? PropertyListDecoder().decode([Alert].self, from: data) else { return []}
+        return alerts
     }
 }
 
 //UITableView Datasource, Delegate
 extension AlertListViewContoller {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alertList.count
+        return alerts.count
     }
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
@@ -39,9 +70,11 @@ extension AlertListViewContoller {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlertListCell", for: indexPath) as? AlertListCell else { return UITableViewCell() }
         
-        cell.alertSwitch.isOn = alertList[indexPath.row].isOn
-        cell.timeLabel.text = alertList[indexPath.row].time
-        cell.meridiemLabel.text = alertList[indexPath.row].meridiem
+        cell.alertSwitch.isOn = alerts[indexPath.row].isOn
+        cell.timeLabel.text = alerts[indexPath.row].time
+        cell.meridiemLabel.text = alerts[indexPath.row].meridiem
+        
+        cell.alertSwitch.tag = indexPath.row
         
         return cell
     }
@@ -57,8 +90,9 @@ extension AlertListViewContoller {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
-            //노티피케이션 삭제 구현
-            return
+            self.alerts.remove(at: indexPath.row)
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.alerts), forKey: "alerts")
+            self.tableView.reloadData()
         return
         default:
             break
