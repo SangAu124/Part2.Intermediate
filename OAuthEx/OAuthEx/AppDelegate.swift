9e8354f275515ab5112cfc4914a32dc0d94877d3
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-04-26 22:03:03 +0900

    UPDATE :: 주석 추가

--- OAuthEx/OAuthEx/AppDelegate.swift
+++ OAuthEx/OAuthEx/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Firebase 초기화
+        FirebaseApp.configure()
+        
+//        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+//        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
+    
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+//        return GIDSignIn.sharedInstance().handle(url)
+//    }
 
     // MARK: UISceneSession Lifecycle
 
--- OAuthEx/OAuthEx/MainViewController.swift
+++ OAuthEx/OAuthEx/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     
@@ -22,8 +23,21 @@ class MainViewController: UIViewController {
         
         navigationController?.navigationBar.isHidden = true
         
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        welcomeLabel.text = """
+            환영합니다.
+            \(email)님
+        """
     }
     @IBAction func logoutButtontapped(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
         self.navigationController?.popToRootViewController(animated: true)
+        
+//        do {
+//            try firebaseAuth.signOut()
+//            self.navigationController?.popToRootViewController(animated: true)
+//        }catch let signOutError as NSError {
+//            print("ERROR: signout \(signOutError.localizedDescription)")
+//        }
     }
 }
